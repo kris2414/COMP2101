d3313e72f84cfa26c303df16f2cfe438d6f9c6ad
kris2414 <krisk2414@gmail.com>
2021-12-01 16:35:55 -0500

    Create Lab3-PowershellData.ps1
    Lab 3 Complete

---
+++ powershell/Lab3-PowershellData.ps1
@@ -0,0 +1,4 @@
+﻿$adapters = get-ciminstance win32_networkadapterconfiguration 
+$adapters | Where-Object ipenabled -EQ 1 | 
+Select-Object Description, Index, IPAddress, IPSubnet, DNSDomain, @{n="DNSServer";e={$_.DNSServerSearchOrder }} |
+Format-Table
